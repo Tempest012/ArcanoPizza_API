786bd60f565dff2a69ce5aee4d8ea7c6a974c13b
Cesar-CAAR <cesar.andres5242@gmail.com>
2026-03-21 18:06:55 -0700

    creando pago en controlador y conectandolo a amgular

---
+++ ArcanoPizza_API.DTOs/CarritoItemSeguroDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArcanoPizza_API.DTOs
+{
+    public class CarritoItemSeguroDto
+    {
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
